a980455abff50153aeee63a40221a3b1fa382a08
livefromthelevel <brian.d.wach@gmail.com>
2024-03-24 17:10:19 -0500

    <Setup simple main handlebars layout and created a first attempt of the homepage view render to 5 most recent posts>

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,5 +2,26 @@ const router = require('express').Router();
 const { Posts, Users, Comments } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    // Get all posts in order of most recent
+    const postsData = await Posts.findAll({
+      attributes: ['id', 'title', 'content', 'date_created'],
+      include: [{ model: Users, attributes: ['username'] }],
+      order: [['date_created', 'ASC']],
+      limit: 5
+    });
+
+    // Serialize data so the template can read it
+    const posts = postsData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      posts,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
