1865c4963c0be1e5a1732802ca33677bb5544006
livefromthelevel <brian.d.wach@gmail.com>
2024-03-28 22:54:02 -0500

    <Added seeds API call to seed remote database>

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const usersRoutes = require('./usersRoutes');
 const postsRoutes = require('./postsRoutes');
 const commentsRoutes = require('./commentsRoutes');
+const seedsRoutes = require('./seedsRoutes');
 
 router.use('/users', usersRoutes);
 router.use('/posts', postsRoutes);
 router.use('/comments', commentsRoutes);
+router.use('/seeds', seedsRoutes);
 
 module.exports = router;
